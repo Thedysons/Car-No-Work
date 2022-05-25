bbf23bec633ad97fa3c4c339b8e848b3c4de7d60
samuelLkennedy <Skennedy349@gmail.com>
2022-05-25 21:58:32 +1000

    hopefully this should fix some stuff
    just changed how navigating works

--- frontend/car-no-work/src/App.js
+++ frontend/car-no-work/src/App.js
@@ -1,10 +1,22 @@
 import React from "react"
+import {Route, Routes, BrowserRouter} from "react-router-dom"
+/*pages */
 import SignUp from "./pages/SignUp"
 import RapProfilePage from "./pages/RapProfilePage"
-export default function App() {   
+import LandingPage from "./pages/LandingPage"
+function App() {   
     return (
-    <div>
-        <RapProfilePage />
-    </div>
+        <BrowserRouter>
+            <div>
+                <Routes>
+                    {/* add routes, unordered atm */}
+                    <Route path="/" element={<RapProfilePage />} />  
+                    <Route path="/LandingPage" element={<LandingPage />} />  
+                    <Route path="/SignUp" element={<SignUp />} /> 
+                </Routes>
+            </div>
+        </BrowserRouter>
+    
     )
 }
+export default App;
