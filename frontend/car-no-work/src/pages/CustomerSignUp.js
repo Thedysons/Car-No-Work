8456112739293501371835c80de847a478b8130f
kn728 <kn728@uowmail.edu.au>
2022-05-26 23:10:41 +1000

    made customer sign up look decent

--- frontend/car-no-work/src/pages/CustomerSignUp.js
+++ frontend/car-no-work/src/pages/CustomerSignUp.js
@@ -5,22 +5,25 @@ export default function CustomerSignUp() {
     return (
         <div className= "pageBackground">
             <div className= "pageLayout">
-                <h1>Sign In</h1>
+                <h1>Sign Up</h1>
                 <p className="signInInstruction"> Please enter your login details so we may retrieve your infomation and assist you.</p>
-                <p className="signInInstruction"> Dont have an account? Sign up</p> 
-                <p className="signInLabel">Email: </p>
-                <input type ="text" className="signInText"></input>
-                <p className="signInLabel">Password: </p>
-                <input type ="text" className="signInText" ></input>
-                <p className="signInLabel">Name: </p>
-                <input type ="text" className="signInText" ></input>
-                <p className="signInLabel">Address: </p>
-                <input type ="text" className="signInText" ></input>
-                <p className="signInLabel">Date of Birth: </p>
-                <input type ="text" className="signInText" ></input>
+                
+                <div className = "signUpStructure">
+                    <p className="signInEmail">Email: </p>
+                    <input type ="text" className="signInText" placeholder="email"></input>
+                    <p className="signInLabel">Password: </p>
+                    <input type ="text" className="signInText" placeholder="password" ></input>
+                    <p className="signInLabel">Name: </p>
+                    <input type ="text" className="signInText" placeholder="name" ></input>
+                    <p className="signInLabel">Address: </p>
+                    <input type ="text" className="signInText"  placeholder="address"></input>
+                    <p className="signInLabel">Date of Birth: </p>
+                    <input type ="text" className="signInText"  placeholder="Date Of Birth"></input>
+                    <button className="createAccount">Create account</button>
+                </div>
                 
                
-                <button className="createAccount">Create account</button>
+                
                 
             </div>
             
